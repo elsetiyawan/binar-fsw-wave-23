289ad5e8fd0298995e08f0a4d0036726dc88a170
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-10-17 20:35:40 +0700

    pagination

--- cp10/belajar-next/pages/users/index.js
+++ cp10/belajar-next/pages/users/index.js
@@ -1,12 +1,32 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 
 const Users = (props) => {
+  const router = useRouter();
+  const currentPage = router.query.start || 0;
+
+  const onClickNext = (start, limit) => {
+    router.push({
+      pathname: router.pathname,
+      query: { start, limit },
+    });
+  };
+
   return (
     <div>
       <div>
         {props.usersList.map((user) => (
           <div key={user.id}>{user.name}</div>
         ))}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            onClickNext(parseInt(currentPage) + 2, 2);
+          }}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
@@ -16,13 +36,21 @@ export default Users;
 
 export async function getServerSideProps(context) {
   const usersList = await axios
-    .get("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.data)
+    .get(
+      `https://jsonplaceholder.typicode.com/users?_start=${
+        context?.query?.start || 0
+      }&_limit=${context?.query?.limit || 2}`
+    )
+    .then((res) => {
+      console.log(res.config.url);
+      return res.data;
+    })
     .catch((err) => []);
 
   return {
     props: {
       usersList,
+      page: 1,
     }, // will be passed to the page component as props
   };
 }
